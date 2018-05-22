a92993aff56c5e55280904d2ca720ba9bf8ae03b
seror <seror94ab@yahoo.com>
2018-05-21 20:43:08 -0500

    displaying table information

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,17 +18,33 @@ connection.connect(function (err) {
     showAllProducts();
 });
 
-inquirer.prompt([{
-    type: "input",
-    name: "product",
-    message: "What is the ID of the item that you would like to buy?",
-    filter: Number
-},
 
-{
-    type: "input",
-    name: "quantity",
-    message: "How many units of this product would you like to buy?",
-    filter: Number
+
+function productInfo() {
+    inquirer.prompt([{
+        type: "input",
+        name: "product",
+        message: "What is the ID of the item that you would like to buy?",
+        filter: Number
+    },
+
+    {
+        type: "input",
+        name: "quantity",
+        message: "How many units of this product would you like to buy?",
+        filter: Number
+    }
+    ])
+
 }
-])
+
+
+function showAllProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log("\nItem Id " + res[i].item_id + "|" + "\nProduct Name: " + res[i].product_name + "|" + "\nDepartment Name: " + res[i].department_name + "|" + "\nPrice: " + res[i].price + "|" + "\nStock Quantity: " + res[i].stock_quantity);
+        }
+        console.log("---------------");
+        productInfo()
+    });
+}
